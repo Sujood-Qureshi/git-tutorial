3c0a3af4e0dd6c92ebf7fe56d86bfdc16ecc295a
Sujood-Qureshi <sujoodqureshi@gmail.com>
2024-12-18 20:20:35 +0530

    changes to remote branch called master

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use('/', (req, res)=>{
-    res.send("Hello World!, changes made on another-branch version 2 here is the change");
+    res.send("Hello World!, making change to the remote branch...");
 })
 
 app.listen(3000, () => {
